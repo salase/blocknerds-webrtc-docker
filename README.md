d94701ca6b3535d59b1582618448cb37eb7aa98b
Eduardo Salas <ezsb82@gmail.com>
2021-09-08 23:57:41 -0300

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # blocknerds-webrtc-docker
+
+## Set up hosts file
+Edit /etc/hosts file to add
+```bash
+127.0.0.1  frontend.test
+127.0.0.1  backend.test
+```
+
+## Set up Certs
+In the cert folder run
+```bash
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout domain-selfsigned.key -out domain-selfsigned.crt
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout local-selfsigned.key -out local-selfsigned.crt
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout public-selfsigned.key -out public-selfsigned.crt
+```
+
+## Set up .env file with your local paths
+
+```bash
+cp .env.example .env
+```
+
+Edit your Yii project path as needed
+
+## Run
+```bash
+./start.sh
+```
---
+++ start.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+clear
+
+# exit the script if any statement returns a non-true return value
+set -e
+
+INFO='\033[0;34m'    # Blue
+ALERT='\033[0;31m'   # Redr
+NC='\033[0m'         # No Color
+
+CERT=""
+
+function checks() {
+    if [ -x "$(command -v docker)" ]; then
+        echo -e "${INFO}Docker is intalled...${NC}"
+    else
+        echo -e "${ALERT}Please install docker!${NC}"
+        exit 1
+    fi
+
+    if [ -x "$(command -v docker-compose)" ]; then
+        echo -e "${INFO}Docker compose is intalled...${NC}"
+    else
+        echo -e "${ALERT}Please install docker-compose!${NC}"
+        exit 1
+    fi
+
+    if [ -f "docker-compose.yml" ]; then
+        echo -e "${INFO}docker-compose.yml file exists...${NC}"
+    else
+        echo -e "${ALERT}docker-compose.yml file does not exist!${NC}"
+        exit 1
+    fi
+
+    if [ -x "$(command -v git)" ]; then
+        echo -e "${INFO}Git is intalled...${NC}"
+    else
+        echo -e "${ALERT}Please install git!${NC}"
+        exit 1
+    fi
+}
+
+function select_cert() {
+    echo -e "${INFO}Select certificate and press enter:${NC}"
+
+    echo "  1) Local: 192.168.0.2"
+    echo "  2) Public: 1.2.3.4"
+    echo "  3) Domain: example.com"
+    echo "  4) Exit"
+
+    read n
+
+    case $n in
+    1)
+        CERT="local"
+        ;;
+    2)
+        CERT="public"
+        ;;
+    3)
+        CERT="domain"
+        ;;
+    *) 
+        echo -e "${ALERT}Invalid option $REPLY ${NC}" 
+        exit 1
+        ;;
+    esac
+}
+
+checks
+select_cert
+
+if [ -n "${CERT}" ]; then
+    #sudo sysctl -w vm.max_map_count=262144
+    docker stop $(docker ps -aq)
+    CERT=$CERT docker-compose up --build
+    #let containers=$(docker ps -aq)
+    #if [[ ! -z "containers" ]]; then
+    #    CERT=$CERT docker-compose up --build
+    #fi
+fi
+
